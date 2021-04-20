8a1a9e0e7c2eda6c190cbf82c1005d632959258c
GMChristo <gabrielchristo@hotmail.com>
2021-04-20 16:51:38 -0300

    Criando card movie

--- lib/src/modules/components/components/tmdb_api/tmdb_api.dart
+++ lib/src/modules/components/components/tmdb_api/tmdb_api.dart
@@ -14,18 +14,28 @@ import 'package:tbdb_teste_api_front/src/modules/components/repositories/movie_r
 ])
 class TmdbApi implements OnInit {
   // String url = 'https://api.themoviedb.org/3/search/movie?api_key=6c339271cfd6ae9c2ccd3410abd4048c&language=pt-BR&query=Senhor dos aneis&page=1&include_adult=false';
-  String title;
+  String movieName;
   List<Movie> movies;
+  bool carregou = false;
 
-  Future<void> getAll() async {
-    movies = await MovieRepository().findMovie();
+  Future<void> getAll(movieName) async {
+    movies = await MovieRepository().findMovie(movieName);
     for (var m in movies) {
       print(m.title);
+    }
+    carregou = true;
+  }
+
+  void load() {
+    if(carregou == false){
+      print('teste load');
+    }else{
+    getAll(movieName);
     }
   }
 
   @override
   void ngOnInit() async {
-    await getAll();
+    load();
   }
 }
--- lib/src/modules/components/repositories/movie_repository.dart
+++ lib/src/modules/components/repositories/movie_repository.dart
@@ -1,13 +1,12 @@
 import 'package:dio/dio.dart';
-import 'package:essential_rest/essential_rest.dart';
 import 'package:tbdb_teste_api_front/src/model/movie.dart';
 
 class MovieRepository {
   MovieRepository();
 
-  Future<List<Movie>> findMovie() async {
+  Future<List<Movie>> findMovie(String movieName) async {
     var url =
-        'https://api.themoviedb.org/3/search/movie?api_key=6c339271cfd6ae9c2ccd3410abd4048c&language=pt-BR&query=Senhor dos aneis&page=1&include_adult=false';
+        'https://api.themoviedb.org/3/search/movie?api_key=6c339271cfd6ae9c2ccd3410abd4048c&language=pt-BR&query=$movieName&page=1&include_adult=false';
     var dio = Dio();
     var response = await dio.get(url);
     if (response.statusCode == 200) {
